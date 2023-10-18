e06b051da0b1383cd4855529636dd61652a7b7c5
Shilpa Akula <shilpaa661@gmail.com>
2023-10-18 15:06:58 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 use leptos::*;
 
-fn main() {
-    mount_to_body(|| view! { <p>"Hello, world!"</p> })
+
+
+use aws_lambda_events::{
+    apigw::ApiGatewayProxyResponse, encodings::Body,
+    event::apigw::ApiGatewayProxyRequest,
+};
+use http::HeaderMap;
+use lambda_runtime::{service_fn, Error, LambdaEvent};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    lambda_runtime::run(service_fn(handler)).await?;
+    Ok(())
+}
+
+async fn handler(
+    _: LambdaEvent<ApiGatewayProxyRequest>,
+) -> Result<ApiGatewayProxyResponse, Error> {
+    Ok(ApiGatewayProxyResponse {
+        status_code: 200,
+        headers: HeaderMap::new(),
+        multi_value_headers: HeaderMap::new(),
+        body: Some(Body::Text(
+            "🦀🦀🦀🦀🦀🦀 Hello, Rust 🦀🦀🦀🦀🦀🦀"
+                .to_string(),
+        )),
+        is_base64_encoded: Some(false),
+    })
 }
